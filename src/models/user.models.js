69819a16e127d4b6c47b410819a85f8ce6d40468
Krish <140335688+Graviton17@users.noreply.github.com>
2025-01-28 17:16:13 +0530

    Add user password hashing and token generation methods; update dependencies

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrpt from "bcrypt";
+import { jwtDecode } from "jwt-decode";
 
 // schema is a blueprint of how the data will look like
 const userSchema = new Schema(
@@ -50,5 +52,35 @@ const userSchema = new Schema(
         timestamps: true
     }
 );
+
+// before saving the user, hash the password
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) { // if password is not modified, do nothing
+        next();
+    }
+
+    this.password = await bcrpt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.comparePassword = async function (password) { // add method to the schema of comparing password
+    return await bcrpt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = async function () { // add method to the schema of Access generating token
+    return await jwtDecode.sign(
+        { _id: this._id },
+        process.env.Access_Token_Secret,
+        { expiresIn: Access_Token_Expiry }
+    );
+}
+
+userSchema.methods.generateRefreshToken = async function () { // add method to the schema of generating refresh token
+    return await jwtDecode.sign(
+        { _id: this._id },
+        process.env.Refresh_Token_Secret,
+        { expiresIn: Refresh_Token_Expiry }
+    );
+}
 
 export const User = mongoose.model("User", userSchema); // model is a wrapper for the schema
